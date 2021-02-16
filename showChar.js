2a486f6839c12d8981342a10b43b143cf6b9334b
Futursaith <saith_7@hotmail.fr>
2021-02-16 18:59:47 +0100

    exo 4/5

---
+++ showChar.js
@@ -0,0 +1,6 @@
+let text = `C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.`
+
+console.log(`Gustave Flaubert extrait du livre salammbô\nNombre de charactère : ${text.length}`)
+
+for (let i = 0; i < text.length; i++)
+  console.log(`Le caractère ${text[i]} est à l'index ${i}`)
